edd15b91570e6e3494807c79e0c4ffc257280603
Eric Dapp <eric.dapp.perso@gmail.com>
2013-06-25 14:39:13 +0200

    Update DS1307.h
    - Add the time attributs
    - Add the time get/set functions

--- 02_Sources/DS1307/DS1307.h
+++ 02_Sources/DS1307/DS1307.h
@@ -40,14 +40,45 @@ class DS1307 {
 	
 		DS1307(Print &_serial);	// Constructor 
 		
-		void init(void);			// Initialization function
+		void init(void);				// Initialization function
+		
+		
 		
 	private:
 			// Attributs
-		Print* serial;
+		Print* 		serial;
+		
+		uint8_t 	second;				// Time components
+    	uint8_t 	minute;
+   		uint8_t 	hour; 
+    	uint8_t 	day_Of_Week;		// Day of Week, 0 = sunday - TBC
+    	uint8_t 	day;
+    	uint8_t 	month;
+    	uint16_t 	year
 			
 			// Fonctions
-		uint8_t is_Present(void);	// Check that the RTS is present
+		uint8_t 	is_Present(void);	// Check that the RTS is present
+		
+		uint8_t 	get_Second 	();		// Get/Set Second
+		void 		set_Second 	();
+		
+		uint8_t 	get_Minute 	();		// Get/Set Minute
+		void 		set_Minute 	();
+		
+		uint8_t 	get_Hour 	();		// Get/Set Hour	
+		void 		set_Hour 	();
+		
+		uint8_t 	get_Day_Of_Week	();	// Get/Set Day Of Week
+		void 		set_Day_Of_Week ();
+		
+		uint8_t 	get_Day 	();		// Get/Set Day
+		void 		set_Day 	();
+		
+		uint8_t 	get_Month 	();		// Get/Set Month
+		void 		set_Month 	();
+		
+		uint16_t	get_Year	();		// Get/Set Year
+		uint16_t	set_Year	();
 };
 
 #endif
